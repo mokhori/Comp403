71697c0480c01dd3f4911d9fa112313ba4932026
Mohammadreza <mo.khorshidi80@gmail.com>
2024-11-04 13:20:19 +0330

    01 session

---
+++ 01/HelloWorld.cpp
@@ -0,0 +1,44 @@
+// #include <Arya.h> //we can create our own library
+// #include<cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // one line comment
+    /*
+        muli line
+        comments
+    */
+
+    // printf("hello world!"); // C version
+    std::cout << "hello world!";
+    std::cout << "My name is \n mohammadreza";
+
+    /*std::cout<<"My name is";
+    std::cout<<std::endl;
+    std::cout<<"Mohammadreza";*/
+
+    cout << "My name is";
+    cout << endl;
+    cout << "Mohammadreza";
+
+    cout << endl << "My name is" << endl << "Mohammadreza";
+
+    cout << "\nMy name is\nMohammadreza";
+
+    cout << "\n\"hello\tali\"" << endl;
+
+    cout << "3" << endl;
+    cout << 3 << endl;
+
+    cout << 3 * 4 << endl;
+    cout << 3 + 4 << endl;
+    cout << 4 - 3 << endl;
+    cout << 4 % 3 << endl;
+
+    cout << "3+4=" << 3 + 4;
+
+    return 0;
+}
